e16798a3601ceb978221617ef8fc9868907d8108
mark s <masaltzman@gmail.com>
2019-05-30 12:28:22 -0400

    first commit

---
+++ precache-manifest.821e8407f4c912d549a44a8c1fedf798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4ea9f169da015c18132b0e9f89ae30",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "9120f9b3560787003e8d",
+    "url": "/countries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/countries/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "9120f9b3560787003e8d",
+    "url": "/countries/static/js/main.a688b30a.chunk.js"
+  },
+  {
+    "revision": "84c1202aabc4d9b174af",
+    "url": "/countries/static/js/runtime~main.30bae189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countries/static/media/logo.5d5d9eef.svg"
+  }
+]);
